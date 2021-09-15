5025d77e2cd5f0dcf7b76c35ff49cc572ee341a6
Jonathan Callahan <jonathan.s.callahan@gmail.com>
2021-09-15 15:41:53 -0700

    feat: first small changes test

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // roll_median
 Rcpp::NumericVector roll_median(Rcpp::NumericVector x, unsigned int n, Rcpp::Nullable<Rcpp::NumericVector> weights, int by, int align);
 RcppExport SEXP _MazamaRollUtils_roll_median(SEXP xSEXP, SEXP nSEXP, SEXP weightsSEXP, SEXP bySEXP, SEXP alignSEXP) {
